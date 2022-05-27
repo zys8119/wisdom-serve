c98eafd6e8b556add42291ee04910a24128516a1
zys8119 <770959294@qq.com>
2022-05-27 09:33:17 +0800

    [add] websocket

--- src/core-plug/websocket.ts
+++ src/core-plug/websocket.ts
@@ -1,5 +1,6 @@
 import {Plugin} from "@wisdom-serve/serve/types/type";
 import {createHash} from "crypto";
+import * as ncol from "ncol";
 
 /**
  * 解码数据
@@ -76,44 +77,74 @@ const encodeWsFrame = (data)=> {
     return frame;
 }
 
-
 const websocket:Plugin = function ({url, headers,socket}){
     if(/^\/websocket/.test(url) && headers['upgrade'] === 'websocket'){
         if (headers['sec-websocket-version'] !== '13') {
             // 判断WebSocket版本是否为13，防止是其他版本，造成兼容错误
             Promise.reject('WebSocket版本错误')
         } else {
-            // 校验Sec-WebSocket-Key，完成连接
-            /*
-              协议中规定的校验用GUID，可参考如下链接：
-              https://tools.ietf.org/html/rfc6455#section-5.5.2
-              https://stackoverflow.com/questions/13456017/what-does-258eafa5-e914-47da-95ca-c5ab0dc85b11-means-in-websocket-protocol
-            */
-            const GUID = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
-            const key = headers['sec-websocket-key'];
-            const hash = createHash('sha1')  // 创建一个签名算法为sha1的哈希对象
-            hash.update(`${key}${GUID}`)  // 将key和GUID连接后，更新到hash
-            const result = hash.digest('base64') // 生成base64字符串
-            const header = `HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-Websocket-Accept: ${result}\r\n\r\n` // 生成供前端校验用的请求头
-            socket.write(header)  // 返回HTTP头，告知客户端校验结果，HTTP状态码101表示切换协议：https://httpstatuses.com/101。
-            // 若客户端校验结果正确，在控制台的Network模块可以看到HTTP请求的状态码变为101 Switching Protocols，同时客户端的ws.onopen事件被触发。
-            socket.on('data', (buffer) => {
-                const data = decodeWsFrame(buffer);
-                // opcode为8，表示客户端发起了断开连接
-                if (data.opcode === 8) {
-                    socket.end()  // 与客户端断开连接
-                } else {
-                    const payloadDataStr = data.payloadData.toString();
-                    console.log(payloadDataStr)
-                    socket.write(encodeWsFrame({
-                        payloadData: "撒打算大撒大的65656656",
-                        opcode:1
-                    }))
+            return new Promise((resolve, reject) => {
+                // 校验Sec-WebSocket-Key，完成连接
+                /*
+                  协议中规定的校验用GUID，可参考如下链接：
+                  https://tools.ietf.org/html/rfc6455#section-5.5.2
+                  https://stackoverflow.com/questions/13456017/what-does-258eafa5-e914-47da-95ca-c5ab0dc85b11-means-in-websocket-protocol
+                */
+                const GUID = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+                const key = headers['sec-websocket-key'];
+                const hash = createHash('sha1')  // 创建一个签名算法为sha1的哈希对象
+                hash.update(`${key}${GUID}`)  // 将key和GUID连接后，更新到hash
+                const result = hash.digest('base64') // 生成base64字符串
+                const header = `HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-Websocket-Accept: ${result}\r\n\r\n` // 生成供前端校验用的请求头
+                socket.write(header)  // 返回HTTP头，告知客户端校验结果，HTTP状态码101表示切换协议：https://httpstatuses.com/101。
+                if(this.options.debug){
+                    ncol.color(function (){this.log(`【websocket】：`).info(`websocket版本->sec-websocket-version：${headers['sec-websocket-version']}`)})
+                    ncol.color(function (){this.log(`【websocket】：`).info(`websocket连接成功->客户端密钥：${key}`)})
                 }
+                // 若客户端校验结果正确，在控制台的Network模块可以看到HTTP请求的状态码变为101 Switching Protocols，同时客户端的ws.onopen事件被触发。
+                socket.on('data', (buffer) => {
+                    const data = decodeWsFrame(buffer);
+                    // opcode为8，表示客户端发起了断开连接
+                    if (data.opcode === 8) {
+                        socket.end()  // 与客户端断开连接
+                    } else {
+                        const payloadDataStr = data.payloadData.toString();
+                        socket.write(encodeWsFrame({
+                            payloadData: payloadDataStr,
+                            opcode:1
+                        }))
+                    }
+                })
+                socket.on("close",hadError => {
+                    socket.end()
+                    if(this.options.debug) ncol.error(hadError)
+                    reject("连接已关闭")
+                })
+                socket.on("error",hadError => {
+                    socket.end()
+                    if(this.options.debug) ncol.error(hadError)
+                    reject("连接错误")
+                })
+                socket.on("timeout",()=> {
+                    socket.end()
+                    if(this.options.debug) ncol.error("连接超时")
+                    reject("连接超时")
+                })
+                socket.on("end",()=> {
+                    socket.end()
+                    if(this.options.debug) ncol.error("连接关闭")
+                    reject("连接关闭")
+                })
             })
         }
         return Promise.reject(false)
     }
     return Promise.resolve()
 }
+
 export default websocket
+
+declare module "@wisdom-serve/serve" {
+    interface AppServeInterface {
+    }
+}
