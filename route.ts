d7b3da0b76636c7c1d0ae3542c8fc11dc1c8d683
zys8119 <770959294@qq.com>
2022-06-10 21:33:11 +0800

    [add] wisdom-serve

--- route.ts
+++ route.ts
@@ -4,45 +4,7 @@ export default createRoute({
         {
             path:"/",
             controller:async function (){
-                const pageNo = Number(this.$Serialize.get(this.$query,"pageNo", 1));
-                const pageSize = Number(this.$Serialize.get(this.$query,"pageSize", 15));
-                const search = String(this.$Serialize.get(this.$query,"search",''));
-                const p = await this.$DBModel.createSQL({
-                    from:true,
-                    gather:await this.$DBModel.tables.test2.get(true),
-                    gather_alias:"a",
-                    left_join:await this.$DBModel.createSQL({
-                        gather:await this.$DBModel.tables.test3.get(true),
-                        gather_alias:'b',
-                    }),
-                    where:{
-                        'a.vs':{
-                            like:`%${search}%`
-                        }
-                    },
-                    on: {
-                        'a.id':{
-                            source:true,
-                            value:"b.tid",
-                        },
-                    }
-                })
-                const a = await this.$DBModel.runSql(await this.$DBModel.createSQL({
-                    select:["tid", 'b.id', 'a.id as c', 'b.*', 'a.*'],
-                    gather_alias:p as any,
-                    limit:[(pageNo - 1)*pageSize, pageSize]
-                }),"联表查询",'表 test2、test3')
-                const {results:total} = await this.$DBModel.runSql(await this.$DBModel.createSQL({
-                    count:true,
-                    gather_alias:p as any,
-                }),"联表查询总数",'表 test2、test3')
-                // 序列化数据
-                this.$success({
-                    list:a.results,
-                    pageNo,
-                    pageSize,
-                    total:(total[0] || {}).total
-                })
+                this.$success()
             },
             children:[
                 {
