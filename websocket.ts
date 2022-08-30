869ee29f0b2edc74228b61a0386527971b4820a7
zys8119 <770959294@qq.com>
2022-08-30 13:33:54 +0800

    [add] websocket.ts

--- websocket.ts
+++ websocket.ts
@@ -1,25 +1,5 @@
 import {Websocket} from "@wisdom-serve/serve/types/type";
 
 export default <Partial<Websocket>>{
-    login({payload, key}){
-        console.log("登陆成功：",payload)
-        this.$socketList[key].payload = payload;
-    },
-    uploadFile({payload}){
-        const webview = Object.values(this.$socketList).find((e)=>{
-            return e.payload.webview_id === payload.id
-        })
-        if(webview){
-            console.log(payload, webview.payload)
-            this.$socketSend(JSON.stringify(payload),[webview.key])
-        }
-    },
-    uploadFileSelect({payload}){
-        const program = Object.values(this.$socketList).find((e)=>{
-            return e.payload.id === payload.webview_id
-        })
-        if(program){
-            this.$socketSend(JSON.stringify(payload),[program.key])
-        }
-    }
+
 }
