8f3a543d20dbb49930d3ce59f7dcdbfefcb697e1
zys8119 <770959294@qq.com>
2023-02-17 16:05:47 +0800

    [add] openai

--- route.ts
+++ route.ts
@@ -1,19 +1,40 @@
 import {createRoute} from "@wisdom-serve/serve"
+import { Configuration, OpenAIApi } from "openai"
 export default createRoute({
     routes:[
         {
             path:"/",
             controller:async function (){
-                this.$success()
-            },
-            children:[
-                {
-                    path:"api/:aaa/:bbb",
-                    controller:async function(req, res){
-                        this.$success()
+                const configuration = new Configuration({
+                    apiKey:"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",
+                });
+                const openai = new OpenAIApi(configuration);
+                const init = async (result:string = '')=>{
+                    try {
+                        const {data} = await openai.createCompletion({
+                            model: "text-davinci-003",
+                            prompt: result ,
+                            temperature: 0.3,
+                            top_p: 1,
+                            frequency_penalty: 0.0,
+                            presence_penalty: 0.0,
+                        });
+                        if(data.choices.length == 0 || !data.choices[0].text){
+                            return result
+                        }
+                        result = result + data.choices[0].text
+                        console.log(result)
+                        return await init(result)
+                    }catch (e) {
+                        try {
+                            this.$error(e.response.data)
+                        }catch (ee){
+                            this.$error(ee.message)
+                        }
                     }
                 }
-            ]
+                this.$success(await init('介绍流浪地球'))
+            },
         }
     ]
 });
