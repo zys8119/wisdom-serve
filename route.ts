91f7527b4856423ae2dc0189f115a3e9ebaaf3be
zys8119 <770959294@qq.com>
2023-02-20 10:16:50 +0800

    [add] openai

--- route.ts
+++ route.ts
@@ -1,39 +1,62 @@
 import {createRoute} from "@wisdom-serve/serve"
-import { Configuration, OpenAIApi } from "openai"
+import { ConfigurationParameters, Configuration, OpenAIApi, CreateCompletionResponse } from "openai"
+import { merge } from "lodash"
+import {CreateCompletionRequest} from "openai/api";
+import {AxiosRequestConfig} from "axios";
+type InitOptions = Partial<{
+    query:string
+    isContinue:boolean,
+    callback(data:CreateCompletionResponse):void
+    adapter:OpenAIApi
+    configuration:ConfigurationParameters
+    createCompletion:CreateCompletionRequest
+    axiosRequest:AxiosRequestConfig
+}>
+const createCompletion = async (options:InitOptions= {})=>{
+    const config = merge({} as InitOptions, options)
+    config.adapter = config.adapter || new OpenAIApi(new Configuration(config.configuration));
+    const {data} = await config.adapter.createCompletion(merge({
+        model: "text-davinci-003",
+        prompt: config.query ,
+        temperature: 1,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    }, config.createCompletion), merge({
+        timeout:0,
+    }, config.axiosRequest));
+    config.callback?.(data)
+    if(!config.isContinue){config.query = ''}
+    config.query = config.query + data.choices[0].text
+    if(data.choices[0].finish_reason === 'stop'){
+        return  config.query
+    }
+    return await createCompletion(merge(config, {
+        isContinue:true
+    } as InitOptions))
+}
 export default createRoute({
     routes:[
         {
             path:"/",
             controller:async function (){
-                const configuration = new Configuration({
-                    apiKey:"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",
-                });
-                const openai = new OpenAIApi(configuration);
-                const init = async (result:string = '')=>{
+                try {
+                    this.$success(await createCompletion({
+                        query:'农历怎么计算',
+                        callback(data: CreateCompletionResponse) {
+                            console.log(data.choices[0].text)
+                        },
+                        configuration:{
+                            apiKey:"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",
+                        },
+                    }))
+                }catch (e){
                     try {
-                        const {data} = await openai.createCompletion({
-                            model: "text-davinci-003",
-                            prompt: result ,
-                            temperature: 0.3,
-                            top_p: 1,
-                            frequency_penalty: 0.0,
-                            presence_penalty: 0.0,
-                        });
-                        if(data.choices.length == 0 || !data.choices[0].text){
-                            return result
-                        }
-                        result = result + data.choices[0].text
-                        console.log(data.choices[0].text)
-                        return await init(result)
-                    }catch (e) {
-                        try {
-                            this.$error(e.response.data)
-                        }catch (ee){
-                            this.$error(ee.message)
-                        }
+                        this.$error(e.response.data)
+                    }catch (ee){
+                        this.$error(ee.message)
                     }
                 }
-                this.$success(await init('介绍流浪地球'))
             },
         }
     ]
