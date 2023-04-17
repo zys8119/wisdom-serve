2bc2c47d16c29b766f463cc920bff76fbda81714
zys8119 <770959294@qq.com>
2023-04-17 17:28:29 +0800

    [add] Websocket

--- websocket.ts
+++ websocket.ts
@@ -1,5 +1,12 @@
 import {Websocket} from "@wisdom-serve/serve/types/type";
-
 export default <Partial<Websocket>>{
-
+    login({payload, socket}){
+        (socket as any).user = payload
+    },
+    onScroll({payload, send}){
+        send(JSON.stringify(payload))
+    },
+    drawEnd({payload, send}){
+        send(JSON.stringify(payload))
+    }
 }
