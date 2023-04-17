32684da6eb4933fb7def2017a96b49b2e90c3467
zys8119 <770959294@qq.com>
2023-04-17 17:31:40 +0800

    [add] Websocket

--- websocket.ts
+++ websocket.ts
@@ -1,4 +1,13 @@
 import {Websocket} from "@wisdom-serve/serve/types/type";
+type DrawEndMapArray = {
+    index:number
+    total:number
+    page:number
+    img:number
+    id:number
+    emit:string
+}
+const drawEndMap = new Map<number, Array<DrawEndMapArray>>()
 export default <Partial<Websocket>>{
     login({payload, socket}){
         (socket as any).user = payload
@@ -7,6 +16,24 @@ export default <Partial<Websocket>>{
         send(JSON.stringify(payload))
     },
     drawEnd({payload, send}){
+        if(!drawEndMap.has(payload.id)){
+            drawEndMap.set(payload.id, [])
+        }
+        const currChunk = drawEndMap.get(payload.id)
+        currChunk.push(payload)
+        currChunk[payload.index] = payload
+        if(currChunk.length === payload.total){
+            const img = currChunk.map(e=>e.img).join('')
+            payload.img = img
+            drawEndMap.delete(payload.id)
+        }
         send(JSON.stringify(payload))
+        send(JSON.stringify({
+            id:payload.id,
+            index:payload.index,
+            total:payload.total,
+            page:payload.page,
+            emit:'drawEndSuccess',
+        }))
     }
 }
