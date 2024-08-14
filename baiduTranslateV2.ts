9bf45eb181e719ede9faff0b1d181b28125c2247
张云山 <770959294@qq.com>
2024-08-14 16:04:07 +0800

    Create baiduTranslateV2.ts

---
+++ baiduTranslateV2.ts
@@ -0,0 +1,34 @@
+import {Controller} from "@wisdom-serve/serve";
+import {launch} from "puppeteer"
+/**
+ * bobplugin 翻译插件爬虫
+ */
+export default (async function (){
+    try {
+        const url =`https://fanyi.baidu.com/#${this.$body.source_lang.toLowerCase()}/${this.$body.target_lang.toLowerCase()}/${encodeURIComponent(this.$body.text)}`
+        const browser = global.browser || await launch({})
+        const page = global.page || await browser.newPage()
+        await page.goto(url)
+        const selector = '#trans-selection'
+        await page.waitForSelector(selector)
+        const result = await page.evaluate((selector)=>{
+            return (document.querySelector(selector) as HTMLDivElement)?.innerText
+        }, selector)
+        global.browser = browser
+        global.page = page
+        // await browser.close()
+        this.$send(JSON.stringify({
+            "code": 0,
+            "translateResult": [
+                [
+                    {
+                        tgt:result,
+                    }
+                ]
+            ],
+            "type": "zh-CHS2en"
+        }))
+    }catch (e){
+        this.$error()
+    }
+}) as Controller
