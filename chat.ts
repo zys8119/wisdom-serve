a94de8e79f14fbdfc0494fe51be66b73b9b6edc2
zys8119 <770959294@qq.com>
2024-10-09 17:14:07 +0800

    [add] RAG

--- chat.ts
+++ chat.ts
@@ -3,8 +3,18 @@ import { DBSql } from "@wisdom-serve/core-plug/mysql"
 import sql from "./sql-commit-function"
 import ollama from 'ollama'
 export const chat = (async function () {
-    const info:any = {
-        userMessage:[]
+    let body = {
+        tags: [],
+        modelValue: []
+    }
+    try {
+        body = JSON.parse(decodeURIComponent(atob(this.$query.get('data'))))
+    } catch (e) {
+        //
+    }
+    console.log(body)
+    const info: any = {
+        userMessage: []
     }
     const sqls = sql("./sql.sql")
     const db = new DBSql(this, this.request, this.response, '242', {
@@ -16,25 +26,42 @@ export const chat = (async function () {
     })
     const typeMap = {
         // 获取会议信息
-        async m_id({value:conference_id}:any){
-            const {results} = await db.query(sqls.conf_base_info,[conference_id])
+        async m_id({ value: conference_id }: any) {
+            const { results } = await db.query(sqls.conf_base_info, [conference_id])
             info.conference_info = results[0] || {}
         },
-        async quick({prompt}:any){
+        async quick({ prompt }: any) {
             info.userMessage.push({ role: 'user', content: prompt },)
         }
     }
-    await Promise.all(this.$body.tags.map(async (item:any)=>{
+    await Promise.all(body.tags.map(async (item: any) => {
         return typeMap[item.type] && await typeMap[item.type]?.(item)
     }))
-    const response = await ollama.chat({
+    const response: any = await ollama.chat({
+        stream: true,
         model: 'llama3.1',
         messages: [
             { role: 'assistant', content: '你是会议助手，擅长处理会议相关问题及整理相关信息' },
             { role: 'system', content: JSON.stringify(info.conference_info) },
             ...info.userMessage,
-            { role: 'user', content: this.$body.modelValue || '' },
+            { role: 'user', content: body.modelValue || '' },
         ],
-      })
-    this.$success(response.message.content) 
+    })
+    this.response.writeHead(200, {
+        'Content-Type': 'text/event-stream; charset=utf-8',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+
+    // 终止事件发送的条件
+    this.response.on('close', () => {
+        this.response.end(); // 关闭响应
+    });
+    for await (const part of response) {
+        if(part.done){
+            this.response.end(); // 关闭响应
+        }else{
+            this.response.write(`data: ${JSON.stringify(part)}\n\n`)
+        }
+    }
 }) as Controller
