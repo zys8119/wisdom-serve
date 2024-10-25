8976694ffa9a1c6d16aa267fb682c7b9de1e31db
zys8119 <770959294@qq.com>
2024-10-25 15:10:33 +0800

    [add] history

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
 import { createApp } from "@wisdom-serve/serve";
 import "./global";
 import websocket from "./websocket";
-const xhr2 = require("xhr2");
-globalThis.XMLHttpRequest = xhr2;
 createApp({
   route: () => import("./route"),
   websocket,
