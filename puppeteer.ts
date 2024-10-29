fc596b67007ff9600e3380821ef0e7ad548b66bd
zys8119 <770959294@qq.com>
2024-10-29 17:08:38 +0800

    [add] puppeteer

--- puppeteer.ts
+++ puppeteer.ts
@@ -1,7 +1,10 @@
 import {Controller} from "@wisdom-serve/serve"
 import {launch} from "puppeteer"
 export const puppeteer = (async function () {
-    const browser = await launch()
+    const browser = await launch({
+        executablePath:'/usr/bin/chromium-browser',
+        args:['--no-sandbox']
+    })
     const page = await browser.newPage()
     await page.goto("https://www.baidu.com")
     const html = await page.evaluate(async ()=>{
