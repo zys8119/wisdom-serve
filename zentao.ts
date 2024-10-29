5479046121e3dda515c2a9d708334f287b81c2a1
zys8119 <770959294@qq.com>
2024-10-29 16:11:03 +0800

    [add] delete watch file

--- zentao.ts
+++ zentao.ts
@@ -16,7 +16,7 @@ export const zentao = async function (){
         .digest('base64');
     try {
         const bugRes = await axios({
-            url:'http://127.0.0.1:81/zentao-login',
+            url:'http://127.0.0.1:82/zentao-login',
             method:"get",
             params:{
                 account:"admin",
