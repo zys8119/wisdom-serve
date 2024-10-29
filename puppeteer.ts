010827dc920d2b287ed47b1a414f552ff92a6c02
zys8119 <770959294@qq.com>
2024-10-29 16:57:38 +0800

    [add] puppeteer

--- puppeteer.ts
+++ puppeteer.ts
@@ -1,4 +1,12 @@
 import {Controller} from "@wisdom-serve/serve"
+import {launch} from "puppeteer"
 export const puppeteer = (async function () {
-    this.$success()
+    const browser = await launch()
+    const page = await browser.newPage()
+    await page.goto("https://www.baidu.com")
+    const html = await page.evaluate(async ()=>{
+        return document.body.innerHTML
+    })
+    await browser.close()
+    this.$success(html)
 } as Controller)
