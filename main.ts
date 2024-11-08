53437766086c05b3f6ada26b49aff5272e63627d
zys8119 <770959294@qq.com>
2024-11-08 18:06:55 +0800

    [add] rag new

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import { createApp } from "@wisdom-serve/serve";
 import "./global";
 import websocket from "./websocket";
 import "./fetch";
+import "./cron";
 createApp({
   route: () => import("./route"),
   websocket,
