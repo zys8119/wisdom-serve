389a5a89574ff176d47fffd60b6a8640874dcb35
zys8119 <770959294@qq.com>
2026-06-18 23:27:35 +0800

    重构 WebSocket 处理逻辑，优化消息解码和编码功能

--- route.ts
+++ route.ts
@@ -1,27 +1,52 @@
-import {createRoute} from "@wisdom-serve/serve"
+import { createRoute } from "@wisdom-serve/serve";
+import Anthropic from "@anthropic-ai/sdk";
+const anthropic = new Anthropic({
+  baseURL: "http://localhost:11434",
+  apiKey: "ollama", // required but ignored
+});
 export default createRoute({
-    routes:[
-        {
-            path:"/",
-            controller:async function (){
-                // this.$success()
-            },
-            children:[
-                {
-                    path:"api/:aaa/:bbb",
-                    controller:async function(req, res){
-                        this.$success()
-                    }
-                },
-                {
-                    path:"express/.*/:b",
-                    // strict:false,
-                    controller:async function(req, res){
-                        console.log(this.$params)
-                        this.$success({s:44555})
-                    }
-                }
-            ]
+  routes: [
+    {
+      path: "/v1/messages",
+      controller: async function () {
+        const system = this.$body.messages.filter(
+          (item: any) => item.role === "system",
+        );
+        const users = this.$body.messages.filter(
+          (item: any) => item.role === "user",
+        );
+        const others = this.$body.messages.filter(
+          (item: any) => !["system", "user"].includes(item.role),
+        );
+        const messages = [].concat(system, users, others);
+        this.$body.messages = messages;
+        console.log(this.$body.messages);
+        const stream = await anthropic.messages.stream({
+          ...this.$body,
+          messages,
+          // max_tokens: 262144,
+          // model: "[限时]claude-sonnet-4-6",
+        });
+        this.response.writeHead(200, {
+          "Content-Type": "text/event-stream",
+          "Cache-Control": "no-cache",
+          Connection: "keep-alive",
+          "Access-Control-Allow-Origin": "*",
+        });
+        // 立即发送 headers
+        this.response.flushHeaders?.();
+        for await (const event of stream) {
+          if (
+            event.type === "content_block_delta" &&
+            event.delta.type === "text_delta"
+          ) {
+            process.stdout.write(event.delta.text);
+          }
+          this.response.write(`event: ${event.type}\n`);
+          this.response.write(`data: ${JSON.stringify(event)}\n\n`);
         }
-    ]
+        this.response.end();
+      },
+    },
+  ],
 });
